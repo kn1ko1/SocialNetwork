c750cbc3dc6900e6e580df9c2dc5e53b08fddbaf
Ardek <cheethamthing@gmail.com>
2024-01-15 15:59:35 +0000

    Fix: I dunno

--- models/Message.go
+++ models/Message.go
@@ -1,11 +1,11 @@
 package models
 
 type Message struct {
-	MessageId int
-	Body      string
-	CreatedAt int64
-	SenderId  int
-	TargetId  int
-	Type      string
-	UpdatedAt int64
+	MessageId   int
+	Body        string
+	CreatedAt   int64
+	SenderId    int
+	TargetId    int
+	MessageType string
+	UpdatedAt   int64
 }
