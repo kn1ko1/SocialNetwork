005ac1f2d07de98479987cd07ac9bf80dfd992ec
Matthew Hope <matt@matthewhope.co.uk>
2024-01-21 11:28:51 +0000

    Implement Post.Validate pipeline

--- models/Post.go
+++ models/Post.go
@@ -1,5 +1,7 @@
 package models
 
+import "errors"
+
 type Post struct {
 	PostId    int
 	Body      string
@@ -12,5 +14,21 @@ type Post struct {
 
 func (p *Post) Validate() error {
 	// Validate logic here
+	if p.Body == "" {
+		return errors.New("invalid 'Body' field")
+	}
+	if p.CreatedAt <= 0 {
+		return errors.New("invalid 'CreatedAt' field")
+	}
+	// GroupID can be 0 - i.e. not posted to a Group - but cannot be negative
+	if p.GroupId < 0 {
+		return errors.New("invalid 'GroupId' field")
+	}
+	if p.UpdatedAt < p.CreatedAt {
+		return errors.New("invalid 'UpdatedAt' field")
+	}
+	if p.UserId <= 0 {
+		return errors.New("invalid 'UserId' field")
+	}
 	return nil
 }
