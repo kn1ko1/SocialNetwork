289447f8ac01ae608275fbe8f3f9507ff35fab1c
Matthew Hope <matt@matthewhope.co.uk>
2024-01-21 10:39:47 +0000

    Scaffold main: serve static files & setup router

--- main.go
+++ main.go
@@ -1,11 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"socialnetwork/router"
+)
+
+const (
+	addr = ":8080"
+)
 
 func main() {
 	// Setup serve mux
+	mux := http.NewServeMux()
 	// Host static files
+	serveStaticFiles(mux)
 	// Add handlers to router
+	setupRouter(mux)
 	// Listen and serve
-	fmt.Println("Server listening etc. all that jazz")
+	fmt.Printf("server listening at address %s...", addr)
+	err := http.ListenAndServe(addr, mux)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
+func setupRouter(mux *http.ServeMux) {
+	rt := router.NewRouter()
+	mux.Handle("/", rt)
+}
+
+func serveStaticFiles(mux *http.ServeMux) {
+	fsRoot := http.Dir("./static/")
+	fs := http.FileServer(fsRoot)
+	mux.Handle("/static/", http.StripPrefix("/static/", fs))
 }
