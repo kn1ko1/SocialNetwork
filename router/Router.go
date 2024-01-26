4e09c2162155953ae4d7ffce02f30ac127aa2713
Ardek <cheethamthing@gmail.com>
2024-01-26 13:10:22 +0000

    Refactor: Moved code from handlers to router.go

--- router/Router.go
+++ router/Router.go
@@ -16,6 +16,28 @@ func (rt *Router) AddHandler(exp *regexp.Regexp, h http.Handler) {
 	rt.Handlers[exp] = h
 }
 func (rt *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	// // Get Session Cookie
+	// c, err := r.Cookie("Session")
+	// if err != nil {
+	// 	// Log Error
+	// 	log.Println(err.Error())
+	// 	// Return HTTP Status Unauthorized
+	// 	//
+	// 	// N.B. for simplicity of the example, we are simply returning
+	// 	// an HTTP error. In the actual project, probably a JSON payload.
+	// 	http.Error(w, "unauthorized", http.StatusUnauthorized)
+	// 	return
+	// }
+	// // Authenticate Session Cookie - user variable discarded because user struct not used here...
+	// _, err = auth.AuthenticateSessionCookie(c)
+	// if err != nil {
+	// 	// Same error as above - maker of request is unauthorized
+	// 	log.Println(err.Error())
+	// 	http.Error(w, "unauthorized", http.StatusUnauthorized)
+	// 	return
+	// }
+
 	for exp, h := range rt.Handlers {
 		if exp.MatchString(r.URL.Path) {
 			h.ServeHTTP(w, r)
