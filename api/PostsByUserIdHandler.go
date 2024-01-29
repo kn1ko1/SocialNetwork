8bcaa143cebfbd81765ed2afba4932994402a9ee
Matthew Hope <matt@matthewhope.co.uk>
2024-01-29 12:51:14 +0000

    Fix: PostsByUserIdHandler properly retrieves UserID from cookie

--- api/PostsByUserIdHandler.go
+++ api/PostsByUserIdHandler.go
@@ -4,11 +4,11 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"socialnetwork/auth"
 	"socialnetwork/repo"
-	"strconv"
 )
 
-// Endpoint: /api/users/{userId}/posts
+// Endpoint: /api/users/posts
 // Allowed methods: GET, DELETE
 
 type PostsByUserIdHandler struct {
@@ -23,7 +23,6 @@ func NewPostsByUserIdHandler(r repo.IRepository) *PostsByUserIdHandler {
 // A PostsHandler instance implements the ServeHTTP interface, and thus
 // itself becomes an HTTPHandler
 func (h *PostsByUserIdHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-
 	switch r.Method {
 	case http.MethodGet:
 		h.get(w, r)
@@ -38,51 +37,55 @@ func (h *PostsByUserIdHandler) ServeHTTP(w http.ResponseWriter, r *http.Request)
 }
 
 func (h *PostsByUserIdHandler) get(w http.ResponseWriter, r *http.Request) {
-	queryParams := r.URL.Query()
-	postIdString := queryParams.Get("postId")
-	postId, postIdErr := strconv.Atoi(postIdString)
-	if postIdErr != nil {
-		log.Println("Problem with AtoI postId. ", postIdErr)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	// queryParams := r.URL.Query()
+	// postIdString := queryParams.Get("postId")
+	// postId, postIdErr := strconv.Atoi(postIdString)
+	c, err := r.Cookie("Session")
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "unauthorized", http.StatusUnauthorized)
 		return
 	}
-	userPosts, err := h.Repo.GetPostById(postId)
+	user, err := auth.AuthenticateSessionCookie(c)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "unauthorized", http.StatusUnauthorized)
+		return
+	}
+	userPosts, err := h.Repo.GetPostsByUserId(user.UserId)
 	if err != nil {
 		log.Println("Failed to get posts in GetPostsByIdHandler. ", err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
-
+	w.WriteHeader(http.StatusOK)
 	err = json.NewEncoder(w).Encode(userPosts)
 	if err != nil {
 		log.Println("Failed to encode and write JSON response. ", err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
-
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Here are your posts"))
 }
 
 func (h *PostsByUserIdHandler) delete(w http.ResponseWriter, r *http.Request) {
-
-	// figure out userID
-	queryParams := r.URL.Query()
-	userIDString := queryParams.Get("userID")
-	userID, userIDErr := strconv.Atoi(userIDString)
-	if userIDErr != nil {
-		log.Println("Problem with AtoI userID. ", userIDErr)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	c, err := r.Cookie("Session")
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "unauthorized", http.StatusUnauthorized)
 		return
 	}
-	log.Println("Received delete request for userID:", userID)
-
-	err := h.Repo.DeletePostsByUserId(userID)
+	user, err := auth.AuthenticateSessionCookie(c)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "unauthorized", http.StatusUnauthorized)
+		return
+	}
+	// log.Println("Received delete request for userID:", userID)
+	err = h.Repo.DeletePostsByUserId(user.UserId)
 	if err != nil {
 		log.Println("Failed to delete Posts. ", err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
-
 	w.WriteHeader(http.StatusOK)
 }
