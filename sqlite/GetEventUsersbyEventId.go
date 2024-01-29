08b8426a7d97f48abb42a2dd914bb027e6ba24b7
mfenton <martin_fenton@hotmail.co.uk>
2024-01-29 17:22:15 +0000

    GetEventUsersByEventId added

---
+++ sqlite/GetEventUsersbyEventId.go
@@ -0,0 +1,38 @@
+package sqlite
+
+import (
+	"database/sql"
+	"socialnetwork/models"
+)
+
+func GetEventUsersByEventId(database *sql.DB, eventId int) ([]models.EventUser, error) {
+	rows, err := database.Query("SELECT * FROM EVENT_USERS WHERE EventId = ?", eventId)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var eventUsers []models.EventUser
+
+	for rows.Next() {
+		var eventUser models.EventUser
+		err := rows.Scan(
+			&eventUser.EventUserId,
+			&eventUser.CreatedAt,
+			&eventUser.UpdatedAt,
+			&eventUser.UserId,
+			&eventUser.EventId,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		eventUsers = append(eventUsers, eventUser)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return eventUsers, nil
+}
