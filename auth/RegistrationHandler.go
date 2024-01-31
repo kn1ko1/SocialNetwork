62b69d7a9f311ae0f95636419f30377ce05f4a57
Ardek <cheethamthing@gmail.com>
2024-01-31 16:15:00 +0000

    Feat: Added Error handling to auth layer

--- auth/RegistrationHandler.go
+++ auth/RegistrationHandler.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	utils "socialnetwork/helper"
 	"socialnetwork/models"
 	"socialnetwork/repo"
 
@@ -38,6 +39,7 @@ func (h *RegistrationHandler) post(w http.ResponseWriter, r *http.Request) {
 	err := json.NewDecoder(r.Body).Decode(&user)
 
 	if err != nil {
+		utils.HandleError("Error with Registration.", err)
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
@@ -50,6 +52,7 @@ func (h *RegistrationHandler) post(w http.ResponseWriter, r *http.Request) {
 
 	hashPassword, err := bcrypt.GenerateFromPassword([]byte(user.EncryptedPassword), bcrypt.DefaultCost)
 	if err != nil {
+		utils.HandleError("Error with password encryption", err)
 		http.Error(w, err.Error(), http.StatusBadRequest)
 	}
 
@@ -58,7 +61,7 @@ func (h *RegistrationHandler) post(w http.ResponseWriter, r *http.Request) {
 	log.Println("Received user:", user)
 	// _, err = sqlite.CreateUser(db, user)
 	if err != nil {
-		// fmt.Println("Unable to register a new user in AddUserHandler", err)
+		utils.HandleError("Unable to register a new user in AddUserHandler", err)
 		http.Error(w, "Unable to register a new user", http.StatusBadRequest)
 		return
 	}
