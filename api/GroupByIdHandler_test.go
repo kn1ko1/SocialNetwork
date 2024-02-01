ab447c35ce65811ec38283d0f4fc2370b4c4f112
Michael A <{ID}+{username}@users.noreply.github.com>
2024-02-01 17:28:58 +0000

    test: GroupByIdHandler

---
+++ api/GroupByIdHandler_test.go
@@ -0,0 +1,100 @@
+package api
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGroupByIdHandler_Get(t *testing.T) {
+
+	handler := NewGroupByIdHandler(R)
+	group, _ := handler.Repo.GetGroup(1)
+
+	groupJSON, err := json.Marshal(group)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	URL := "/api/groups/" + fmt.Sprint(group.GroupId)
+
+	// Create a new HTTP request with the encoded JSON as the request body
+	req, err := http.NewRequest(http.MethodGet, URL, bytes.NewBuffer(groupJSON))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a response recorder to capture the response
+	recorder := httptest.NewRecorder()
+
+	// Serve the HTTP request using the handler
+	handler.ServeHTTP(recorder, req)
+
+	// Check the response status code
+	if recorder.Code != http.StatusOK {
+		t.Errorf("Expected status code %d, but got %d", http.StatusOK, recorder.Code)
+	}
+}
+
+func TestGroupByIdHandler_Put(t *testing.T) {
+
+	handler := NewGroupByIdHandler(R)
+	group, _ := handler.Repo.GetGroup(1)
+
+	groupJSON, err := json.Marshal(group)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	URL := "/api/groups/" + fmt.Sprint(group.GroupId)
+
+	// Create a new HTTP request with the encoded JSON as the request body
+	req, err := http.NewRequest(http.MethodPut, URL, bytes.NewBuffer(groupJSON))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a response recorder to capture the response
+	recorder := httptest.NewRecorder()
+
+	// Serve the HTTP request using the handler
+	handler.ServeHTTP(recorder, req)
+
+	// Check the response status code
+	if recorder.Code != http.StatusOK {
+		t.Errorf("Expected status code %d, but got %d", http.StatusOK, recorder.Code)
+	}
+}
+
+func TestGroupByIdHandler_Delete(t *testing.T) {
+
+	handler := NewGroupByIdHandler(R)
+	group, _ := handler.Repo.GetGroup(1)
+
+	groupJSON, err := json.Marshal(group)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	URL := "/api/groups/" + fmt.Sprint(group.GroupId)
+
+	// Create a new HTTP request with the encoded JSON as the request body
+	req, err := http.NewRequest(http.MethodDelete, URL, bytes.NewBuffer(groupJSON))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a response recorder to capture the response
+	recorder := httptest.NewRecorder()
+
+	// Serve the HTTP request using the handler
+	handler.ServeHTTP(recorder, req)
+
+	// Check the response status code
+	if recorder.Code != http.StatusOK {
+		t.Errorf("Expected status code %d, but got %d", http.StatusOK, recorder.Code)
+	}
+}
