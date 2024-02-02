aa12b8406a4819f9b54a482714a20304e54b35f8
mfenton <martin_fenton@hotmail.co.uk>
2024-02-02 12:22:53 +0000

    added testing for postUser and fixed post.go privacy testing

--- models/postUser.go
+++ models/postUser.go
@@ -1,9 +1,41 @@
 package models
 
+import (
+	"errors"
+	"math/rand"
+)
+
 type PostUser struct {
 	PostUserId int
 	CreatedAt  int64
 	PostId     int
 	UpdatedAt  int64
 	UserId     int
+}
+
+func (pu *PostUser) Validate() error {
+	if pu.CreatedAt <= 0 {
+		return errors.New("invalid 'CreatedAt' field")
+	}
+	if pu.PostId <= 0 {
+		return errors.New("invalid 'PostId' field")
+	}
+	if pu.UpdatedAt < pu.CreatedAt {
+		return errors.New("invalid 'UpdatedAt' field. cannot be before 'CreatedAt' field")
+	}
+	if pu.UserId <= 0 {
+		return errors.New("invalid 'UserId' field")
+	}
+	return nil
+}
+
+func GenerateValidPostUser() *PostUser {
+	ctime := rand.Int63n(1000) + 1
+	pu := &PostUser{
+		CreatedAt: ctime,
+		PostId:    rand.Intn(1000) + 1,
+		UpdatedAt: ctime,
+		UserId:    rand.Intn(1000) + 1,
+	}
+	return pu
 }
