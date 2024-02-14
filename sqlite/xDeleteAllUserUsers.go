cc1b801fa4860fbbb41d934a48b6f6015b9e19df
mfenton <martin_fenton@hotmail.co.uk>
2024-02-14 15:39:43 +0000

    added all queries for useruser

---
+++ sqlite/xDeleteAllUserUsers.go
@@ -0,0 +1,16 @@
+package sqlite
+
+import (
+	"database/sql"
+	"socialnetwork/utils"
+)
+
+// Deletes all userusers from the USER_USERS table; use with caution
+func DeleteAllUserUsers(database *sql.DB) error {
+	_, err := database.Exec("DELETE FROM USER_USERS")
+	if err != nil {
+		utils.HandleError("Error executing delete statement.", err)
+		return err
+	}
+	return nil
+}
