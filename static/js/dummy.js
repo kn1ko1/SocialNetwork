b5c18f0de5466a9457fe6fa4e3f2b5ebf473375b
Matthew Hope <matt@matthewhope.co.uk>
2024-02-14 16:36:40 +0000

    Test out dummy websocket JS

--- static/js/dummy.js
+++ static/js/dummy.js
@@ -1,3 +1,30 @@
 window.addEventListener("load", () => {
-    console.log("Hello, World!");
+    const main = document.getElementById("main");
+    const socket = new WebSocket("ws://localhost:8080/ws");
+    socket.onopen = () => {
+        console.log("Web Socket connection established.");
+    }
+    socket.onclose = () => {
+        console.log("Web Socket connection closed.");
+    }
+    socket.onerror = (e) => {
+        console.log(e.data);
+    }
+    socket.onmessage = (e) => {
+        console.log(e.data);
+    }
+    const button = document.createElement("button");
+    button.type = "button";
+    button.innerText = "Click";
+    button.addEventListener("click", () => {
+        const msg = {
+            code: 1,
+            body: JSON.stringify({
+                id: 10,
+                message: "yoyoyoyo"
+            })
+        }
+        socket.send(JSON.stringify(msg))
+    })
+    main.appendChild(button);
 })
