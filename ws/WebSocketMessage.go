5c9f24b14b12fc9e49d782b128372290019796b2
Matthew Hope <matt@matthewhope.co.uk>
2024-02-14 17:50:35 +0000

    Scaffold some message body types

--- ws/WebSocketMessage.go
+++ ws/WebSocketMessage.go
@@ -5,11 +5,13 @@ type WebSocketMessage struct {
 	Body string `json:"body"`
 }
 
+// 1
 type TestBody struct {
 	ID      int    `json:"id"`
 	Message string `json:"message"`
 }
 
+// 2
 type Person struct {
 	Name string `json:"name"`
 	Age  int    `json:"age"`
