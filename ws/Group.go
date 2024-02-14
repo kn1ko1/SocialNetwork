5560bf64f985e40ecc8c78965e2e04edd97df076
Matthew Hope <matt@matthewhope.co.uk>
2024-02-14 14:44:22 +0000

    Prepare to forward Enter/Exit messages to Broadcast Channel

--- ws/Group.go
+++ ws/Group.go
@@ -3,14 +3,16 @@ package ws
 import "fmt"
 
 type Group struct {
+	GroupID   int
 	Clients   map[int]*Client
 	Enter     chan *Client
 	Exit      chan *Client
 	Broadcast chan WebSocketMessage
 }
 
-func NewGroup() *Group {
+func NewGroup(id int) *Group {
 	ret := new(Group)
+	ret.GroupID = id
 	ret.Clients = make(map[int]*Client)
 	ret.Enter = make(chan *Client)
 	ret.Exit = make(chan *Client)
@@ -24,9 +26,11 @@ func (g *Group) Run() {
 		case c := <-g.Enter:
 			g.Clients[c.ClientID] = c
 			fmt.Printf("User %d entered.\n", c.ClientID)
+			// Add to broadcast
 		case c := <-g.Exit:
 			delete(g.Clients, c.ClientID)
 			fmt.Printf("User %d exited.\n", c.ClientID)
+			// Add to broadcast
 		case msg := <-g.Broadcast:
 			for _, c := range g.Clients {
 				c.Send(msg)
