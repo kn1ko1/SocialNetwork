89d14d0f8ef128d6565f27689351b95cce63f7f6
Matthew Hope <matt@matthewhope.co.uk>
2024-02-17 21:14:13 +0000

    Make statement preparation explicit

--- sqlite/DeleteCommentById.go
+++ sqlite/DeleteCommentById.go
@@ -6,8 +6,14 @@ import (
 )
 
 // deletes comments related to CommentId from the COMMENTS table
-func DeleteCommentById(database *sql.DB, commentId int) error {
-	_, err := database.Exec("DELETE FROM COMMENTS WHERE CommentId = ?", commentId)
+func DeleteCommentById(db *sql.DB, commentId int) error {
+	query := `DELETE * FROM "COMMENTS" WHERE "CommentId" = (?)`
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		utils.HandleError("Error preparing delete comment statement.", err)
+		return err
+	}
+	_, err = stmt.Exec(commentId)
 	if err != nil {
 		utils.HandleError("Error executing delete comment statement.", err)
 		return err
