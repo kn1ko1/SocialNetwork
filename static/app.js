2e7bfbf205d5d1d5b459b49fb608b823e711c0bc
Matthew Hope <chickazama@gmail.com>
2024-03-06 15:56:50 +0000

    Bin redundant comments

--- static/app.js
+++ static/app.js
@@ -1,40 +1,3 @@
-// window.addEventListener("load", () => {
-//     renderHomePage();
-// });
-
-// function renderHomePage() {
-//     const main = document.getElementById("main");
-//     main.innerHTML = "";
-//     const h1 = document.createElement("h1");
-//     h1.innerText = "Home";
-//     main.appendChild(h1);
-//     const button = document.createElement("button");
-//     button.innerText = "Other Page";
-//     button.type = "click";
-//     button.addEventListener("click", () => {
-//         renderOtherPage();
-//     })
-//     main.appendChild(button);
-// }
-
-// // Some react function
-// function renderOtherPage(props) {
-//     const main = document.getElementById("main");
-//     main.innerHTML = "";
-//     const h1 = document.createElement("h1");
-//     h1.innerText = "Other";
-//     main.appendChild(h1);
-//     const button = document.createElement("button");
-//     button.innerText = "Home Page";
-//     button.type = "click";
-//     button.addEventListener("click", () => {
-//         renderHomePage();
-//     })
-//     main.appendChild(button);
-// }
-
-// import Login from "./js/Login.js"
-
 const { useState } = React
 
 const App = () => {
@@ -53,13 +16,9 @@ function Login(props) {
 	const [password, setPassword] = useState("")
 	const [redirectVar, setRedirectVar] = useState(false)
 
-	// Redirect
-	// const navigate = useNavigate()
-
 	const submit = async (e) => {
 		e.preventDefault() // prevent reload.
 
-		// Create new user as JS object.
 		const userToLogin = {
 			usernameOrEmail,
 			password,
@@ -78,10 +37,6 @@ function Login(props) {
 		setRedirectVar(true)
 		props.setName(validUser.first)
 	}
-
-	// if (redirectVar) {
-	// 	return navigate("/")
-	// }
 
 	return (
 		<div>
