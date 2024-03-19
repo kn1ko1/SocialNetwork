b2e4a10cb5f19fbdcb12255d191b8de0e38b8045
mfenton <martin_fenton@hotmail.co.uk>
2024-03-19 13:18:46 +0000

    new navbar work

--- static/js/app.js
+++ static/js/app.js
@@ -35,77 +35,79 @@ const App = () => {
 // }
 
 function Navbar(props) {
-  return /*#__PURE__*/React.createElement("nav", {
-    className: "navbar navbar-expand-md bg-body-tertiary"
-  }, /*#__PURE__*/React.createElement("div", {
-    className: "container-fluid"
-  }, /*#__PURE__*/React.createElement("a", {
-    className: "navbar-brand",
-    href: "#"
-  }, "Navbar"), /*#__PURE__*/React.createElement("button", {
-    className: "navbar-toggler",
-    type: "button",
-    "data-bs-toggle": "collapse",
-    "data-bs-target": "#navbarSupportedContent",
-    "aria-controls": "navbarSupportedContent",
-    "aria-expanded": "false",
-    "aria-label": "Toggle navigation"
-  }, /*#__PURE__*/React.createElement("span", {
-    className: "navbar-toggler-icon"
-  })), /*#__PURE__*/React.createElement("div", {
-    className: "collapse navbar-collapse",
-    id: "navbarSupportedContent"
-  }, /*#__PURE__*/React.createElement("ul", {
-    className: "navbar-nav me-auto mb-2 mb-lg-0"
-  }, /*#__PURE__*/React.createElement("li", {
-    className: "nav-item"
-  }, /*#__PURE__*/React.createElement("a", {
-    className: "nav-link active",
-    "aria-current": "page",
-    href: "#"
-  }, "Home")), /*#__PURE__*/React.createElement("li", {
-    className: "nav-item"
-  }, /*#__PURE__*/React.createElement("a", {
-    className: "nav-link",
-    href: "#"
-  }, "Link")), /*#__PURE__*/React.createElement("li", {
-    className: "nav-item dropdown"
-  }, /*#__PURE__*/React.createElement("a", {
-    className: "nav-link dropdown-toggle",
-    href: "#",
-    role: "button",
-    "data-bs-toggle": "dropdown",
-    "aria-expanded": "false"
-  }, "Dropdown"), /*#__PURE__*/React.createElement("ul", {
-    className: "dropdown-menu"
-  }, /*#__PURE__*/React.createElement("li", null, /*#__PURE__*/React.createElement("a", {
-    className: "dropdown-item",
-    href: "#"
-  }, "Action")), /*#__PURE__*/React.createElement("li", null, /*#__PURE__*/React.createElement("a", {
-    className: "dropdown-item",
-    href: "#"
-  }, "Another action")), /*#__PURE__*/React.createElement("li", null, /*#__PURE__*/React.createElement("hr", {
-    className: "dropdown-divider"
-  })), /*#__PURE__*/React.createElement("li", null, /*#__PURE__*/React.createElement("a", {
-    className: "dropdown-item",
-    href: "#"
-  }, "Something else here")))), /*#__PURE__*/React.createElement("li", {
-    className: "nav-item"
-  }, /*#__PURE__*/React.createElement("a", {
-    className: "nav-link disabled",
-    "aria-disabled": "true"
-  }, "Disabled"))), /*#__PURE__*/React.createElement("form", {
-    className: "d-flex",
-    role: "search"
-  }, /*#__PURE__*/React.createElement("input", {
-    className: "form-control me-2",
-    type: "search",
-    placeholder: "Search",
-    "aria-label": "Search"
-  }), /*#__PURE__*/React.createElement("button", {
-    className: "btn btn-outline-success",
-    type: "submit"
-  }, "Search")))));
+  return (
+    /*#__PURE__*/
+    //LOGOUT
+    //NOTIFICATIONS
+    //CHAT
+    //GROUP
+    //HOME
+    //PROFILE
+    React.createElement("nav", {
+      className: "navbar navbar-expand-md bg-body-tertiary"
+    }, /*#__PURE__*/React.createElement("div", {
+      className: "container-fluid"
+    }, /*#__PURE__*/React.createElement("a", {
+      className: "navbar-brand",
+      href: "#"
+    }, "Navbar"), /*#__PURE__*/React.createElement("button", {
+      className: "navbar-toggler",
+      type: "button",
+      "data-bs-toggle": "collapse",
+      "data-bs-target": "#navbarSupportedContent",
+      "aria-controls": "navbarSupportedContent",
+      "aria-expanded": "false",
+      "aria-label": "Toggle navigation"
+    }, /*#__PURE__*/React.createElement("span", {
+      className: "navbar-toggler-icon"
+    })), /*#__PURE__*/React.createElement("div", {
+      className: "collapse navbar-collapse",
+      id: "navbarSupportedContent"
+    }, /*#__PURE__*/React.createElement("ul", {
+      className: "navbar-nav me-auto mb-2 mb-lg-0"
+    }, /*#__PURE__*/React.createElement("li", {
+      className: "nav-item"
+    }, /*#__PURE__*/React.createElement("a", {
+      className: "nav-link",
+      href: "#"
+    }, "LOGOUT")), /*#__PURE__*/React.createElement("li", {
+      className: "nav-item"
+    }, /*#__PURE__*/React.createElement("a", {
+      className: "nav-link",
+      href: "#"
+    }, "NOTIFICATIONS")), /*#__PURE__*/React.createElement("li", {
+      className: "nav-item"
+    }, /*#__PURE__*/React.createElement("a", {
+      className: "nav-link",
+      href: "#"
+    }, "CHAT")), /*#__PURE__*/React.createElement("li", {
+      className: "nav-item"
+    }, /*#__PURE__*/React.createElement("a", {
+      className: "nav-link",
+      href: "#"
+    }, "GROUP")), /*#__PURE__*/React.createElement("li", {
+      className: "nav-item"
+    }, /*#__PURE__*/React.createElement("a", {
+      className: "nav-link",
+      href: "#"
+    }, "HOME")), /*#__PURE__*/React.createElement("li", {
+      className: "nav-item"
+    }, /*#__PURE__*/React.createElement("a", {
+      className: "nav-link",
+      href: "#"
+    }, "PROFILE"))), /*#__PURE__*/React.createElement("form", {
+      className: "d-flex",
+      role: "search"
+    }, /*#__PURE__*/React.createElement("input", {
+      className: "form-control me-2",
+      type: "search",
+      placeholder: "Search",
+      "aria-label": "Search"
+    }), /*#__PURE__*/React.createElement("button", {
+      className: "btn btn-outline-success",
+      type: "submit"
+    }, "Search")))))
+  );
 }
 function Login(props) {
   const [usernameOrEmail, setUsernameOrEmail] = useState("");
