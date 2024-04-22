d9acf90e4cc8d60920b23f60cc11225a9a39656e
Matthew Hope <chickazama@gmail.com>
2024-04-22 14:59:37 +0100

    Move Chat page to own file

--- jsx/app.jsx
+++ jsx/app.jsx
@@ -239,52 +239,6 @@ const renderChat = () => {
 	ReactDOM.render(<Chat />, pageContainer)
 }
 
-function Chat() {
-	const [sendMessage, setSendMessage] = useState("")
-	const [receiveMessage, setReceiveMessage] = useState("")
-
-	let messages = document.getElementById("messages")
-
-	const handleMessages = (e) => {
-		setSendMessage(e.target.value)
-	}
-
-	const handleSubmit = (e) => {
-		e.preventDefault()
-		let bodymessage = { message: sendMessage }
-		let obj = { code: 1, body: JSON.stringify(bodymessage) }
-		socket.send(JSON.stringify(obj))
-		setSendMessage("")
-	}
-
-	socket.onmessage = function (e) {
-		let data = JSON.parse(e.data)
-		let msg = JSON.parse(data.body).message
-		// setReceiveMessage(msg)
-		// console.log("receiveMessage:", receiveMessage)
-		let entry = document.createElement("li")
-		entry.appendChild(document.createTextNode(msg))
-		messages.appendChild(entry)
-	}
-
-	const messageStyle = {
-		color: "orange",
-	}
-
-	return (
-		<div>
-			<h1>Chat</h1>
-			<ul id="messages" style={messageStyle}></ul>
-			<form id="chatbox" onSubmit={handleSubmit}>
-				<textarea onChange={handleMessages}></textarea>
-				<button type="submit" className="btn btn-primary">
-					send
-				</button>
-			</form>
-		</div>
-	)
-}
-
 const renderGroup = () => {
 	const pageContainer = document.querySelector(".page-container")
 	ReactDOM.render(<Group />, pageContainer)
