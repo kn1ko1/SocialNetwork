a0a61b0e677ec7a47cb9d98a63983acb2187220c
Matthew Hope <chickazama@gmail.com>
2024-04-22 15:10:08 +0100

    Build components

--- static/js/app.js
+++ static/js/app.js
@@ -2,6 +2,7 @@ import { Chat } from "./Chat.js";
 import { Profile } from "./Profile.js";
 import { Register } from "./Register.js";
 import { FollowButton } from "./components/FollowButton.js";
+import { GroupDetails } from "./components/GroupDetails.js";
 import { getCurrentUserId } from "./shared/getCurrentUserId.js";
 const {
   useState,
@@ -325,88 +326,6 @@ function Group() {
   }, /*#__PURE__*/React.createElement("h3", null, group.title), /*#__PURE__*/React.createElement("p", null, group.description))) : /*#__PURE__*/React.createElement("div", {
     id: "noGroupsError"
   }, "There are no created groups yet"))));
-}
-function GroupDetails({
-  group
-}) {
-  const [userList, setUserList] = useState([]);
-  const [groupMembers, setGroupMembers] = useState([]);
-  const [groupPosts, setGroupPosts] = useState([]);
-  const [groupMessages, setGroupMessages] = useState([]);
-  const [groupEvents, setGroupEvents] = useState([]);
-  useEffect(() => {
-    const fetchGroupData = async () => {
-      try {
-        const promises = [];
-        promises.push(fetch(`http://localhost:8080/api/users/transport`));
-        promises.push(fetch(`http://localhost:8080/api/groups/${group.groupId}/groupUsers`));
-        promises.push(fetch(`http://localhost:8080/api/groups/${group.groupId}/posts`));
-        promises.push(fetch(`http://localhost:8080/api/groups/${group.groupId}/messages`));
-        promises.push(fetch(`http://localhost:8080/api/groups/${group.groupId}/events`));
-        const results = await Promise.all(promises);
-        const userListResponse = results[0];
-        const groupMembersResponse = results[1];
-        const postsResponse = results[2];
-        const messagesResponse = results[3];
-        const eventsResponse = results[4];
-        if (!userListResponse.ok) {
-          throw new Error('Failed to fetch user list');
-        }
-        if (!groupMembersResponse.ok) {
-          throw new Error('Failed to fetch group members');
-        }
-        if (!postsResponse.ok) {
-          throw new Error('Failed to fetch group posts');
-        }
-        if (!messagesResponse.ok) {
-          throw new Error('Failed to fetch group messages');
-        }
-        if (!eventsResponse.ok) {
-          throw new Error('Failed to fetch group eventsResponse');
-        }
-        const userListData = await userListResponse.json();
-        const groupMembersData = await groupMembersResponse.json();
-        const postsData = await postsResponse.json();
-        const messagesData = await messagesResponse.json();
-        const eventsData = await eventsResponse.json();
-        setUserList(userListData);
-        setGroupMembers(groupMembersData);
-        setGroupPosts(postsData);
-        setGroupMessages(messagesData);
-        setGroupEvents(eventsData);
-      } catch (error) {
-        console.error('Error fetching group posts:', error);
-      }
-    };
-    fetchGroupData();
-  }, [group.groupId]);
-  return /*#__PURE__*/React.createElement("div", {
-    className: "group-details"
-  }, /*#__PURE__*/React.createElement("h2", null, group.title), /*#__PURE__*/React.createElement("p", null, group.description), /*#__PURE__*/React.createElement(PostFormGroup, {
-    groupId: group.groupId
-  }), /*#__PURE__*/React.createElement("div", {
-    className: "userList"
-  }, /*#__PURE__*/React.createElement("h2", null, "UserList"), userList !== null && userList.length > 0 ? userList.map((user, index) => /*#__PURE__*/React.createElement("div", {
-    key: index
-  }, user.username)) : /*#__PURE__*/React.createElement("p", null, "No Users?!")), /*#__PURE__*/React.createElement("div", {
-    className: "groupMembers"
-  }, /*#__PURE__*/React.createElement("h2", null, "Group Members"), groupMembers !== null && groupMembers.length > 1 ? groupMembers.map((member, index) => /*#__PURE__*/React.createElement("div", {
-    key: index
-  }, member.username)) : /*#__PURE__*/React.createElement("p", null, "It's just you... Maybe you should invite someone?")), /*#__PURE__*/React.createElement("div", {
-    id: "groupPosts"
-  }, /*#__PURE__*/React.createElement("h2", null, "Posts"), groupPosts !== null ? groupPosts.map(post => /*#__PURE__*/React.createElement("li", {
-    key: post.createdAt
-  }, post.body)) : /*#__PURE__*/React.createElement("div", {
-    id: "groupPosts"
-  }, "There are no posts in this groups yet")), /*#__PURE__*/React.createElement("div", {
-    className: "groupMessages"
-  }, /*#__PURE__*/React.createElement("h2", null, "Messages"), groupMessages !== null && groupMessages.length > 0 ? groupMessages.map((message, index) => /*#__PURE__*/React.createElement("div", {
-    key: index
-  }, message.body)) : /*#__PURE__*/React.createElement("p", null, "No Messages")), /*#__PURE__*/React.createElement("div", {
-    className: "groupEvents"
-  }, /*#__PURE__*/React.createElement("h2", null, "Events"), groupEvents !== null && groupEvents.length > 0 ? groupEvents.map((event, index) => /*#__PURE__*/React.createElement("div", {
-    key: index
-  }, event.title)) : /*#__PURE__*/React.createElement("p", null, "No Events")));
 }
 const renderNotifications = () => {
   const pageContainer = document.querySelector(".page-container");
