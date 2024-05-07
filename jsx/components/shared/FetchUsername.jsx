6660d21cc4480eee623b85aafc83ba56888b783c
Ardek <cheethamthing@gmail.com>
2024-05-07 19:22:34 +0100

    Fix: git stash didn't take; cleaning up duplicate files

---
+++ jsx/components/shared/FetchUsername.jsx
@@ -0,0 +1,10 @@
+export const fetchUsername = async (userId) => {
+    try {
+        const response = await fetch(`http://localhost:8080/api/users/${userId}`);
+        const data = await response.json();
+        return data.username;
+    } catch (error) {
+        console.error("Error fetching username:", error);
+        return null;
+    }
+};
